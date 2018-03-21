0f08b7fe8604a46871ccb2fa393709872cf3e063
nealzhu <30416461+nealzxz@users.noreply.github.com>
2018-03-20 19:20:49 -0700

    Update parallel.js
    color updated

--- scripts/parallel.js
+++ scripts/parallel.js
@@ -9,9 +9,8 @@ function parallelCoord(data){
 
 	var devicePixelRatio = window.devicePixelRatio || 1;
 
-	var color = d3.scaleOrdinal()
-	  .range(["#5DA5B3","#D58323","#DD6CA7","#54AF52","#8C92E8","#E15E5A","#725D82","#776327","#50AB84","#954D56","#AB9C27","#517C3F","#9D5130","#357468","#5E9ACF","#C47DCB","#7D9E33","#DB7F85","#BA89AD","#4C6C86","#B59248","#D8597D","#944F7E","#D67D4B","#8F86C2"]);
-
+	var color = d3.scaleOrdinal(d3.schemeCategory20);
+	 
 	var types = {
 	  "Number": {
 		key: "Number",
